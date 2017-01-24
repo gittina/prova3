06bd1007e0efd203226a93008e8f2ce9fe9a8748
gittina <martina.paganin95@gmail.com>
2017-01-24 17:45:40 +0100

    ok

--- server/index.js
+++ server/index.js
@@ -267,6 +267,66 @@ app.post('/addStudent', function(request, response)
 
 });
 
+app.post('/searchByMark', function(request, response) 
+{
+	var headers = {};
+	headers["Access-Control-Allow-Origin"] = "*";
+	headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+	headers["Access-Control-Allow-Credentials"] = false;
+	headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+	headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept";
+	headers["Content-Type"] = "application/json";
+
+	var mark;
+	
+	//check body and parameters
+	if ( typeof request.body !== 'undefined' && request.body)
+	{
+		if ( typeof request.body.mark !== 'undefined' && request.body.mark)
+            {
+			 mark = request.body.mark;
+            }
+		else 
+			mark = "not defined";
+	
+	}
+	else
+	{
+		mark = "body undefined";
+	}
+    
+    if ((mark[0]!='<'&&mark[0]!='>')||(isNaN(mark[1]))){
+       mark="not valid";
+    }
+    
+    
+    if (mark!="not defined" && mark!="body undefined" && mark!="not valid")
+	{
+		//aceptable input
+		//search for a student
+		var studentList = studentManager.searchByMark(mark);
+		//if exists
+		if (studentList != null)
+		{
+			response.writeHead(200, headers);
+			response.end(JSON.stringify(studentList));
+		}
+		else
+		{
+			response.writeHead(404, headers);
+			response.end(JSON.stringify());
+		}
+
+	}
+    else    
+	{
+		//unaceptable input
+		response.writeHead(406, headers);
+		response.end(JSON.stringify("1"));
+	}   
+
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
